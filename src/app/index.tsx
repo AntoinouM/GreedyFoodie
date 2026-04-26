3fb9e29897052639abf3621d7af47d86ff059b5f
Antoine Muneret <antoine.muneret2@gmail.com>
2026-04-26 17:58:11 +0200

    feat: add additional text to App component for functionality description

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Food Scanner App 🚀</Text>
+      <Text>Add different tab for different functions</Text>
     </View>
   );
 }
